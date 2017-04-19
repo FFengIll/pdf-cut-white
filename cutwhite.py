7ed8f6672f7589ca38a85b161bab295d9f184b44
FFeng <fengyouzheng@gmail.com>
2017-04-19 15:44:04 +0800

    verified the tool can not work well with pdf which cut by acrobat.

--- cutwhite.py
+++ cutwhite.py
@@ -13,14 +13,11 @@ def cut_box(box, res):
 
     #must translate relative position to absolute position
     bx, by = box.getLowerLeft()
-    bx = float(bx)
-    by = float(by)
+    bx=float(bx);by=float(by)
 
+    print "origin:", box
     box.lowerLeft = (bx + x1, by + y1)
     box.upperRight = (bx + x2, by + y2)
-    print box
-
-
 def cut_white(inputname, outputname='output.pdf'):
     """
     cut the white slide of the input pdf file, and output a new pdf file
@@ -35,11 +32,13 @@ def cut_white(inputname, outputname='output.pdf'):
     for i in range(num):
         scale = pageboxlist[i]
         page = indata.getPage(i)
+        print scale
+
         cut_box(page.mediaBox, scale)
-        #process(page.trimBox,res)
-        #process(page.artBox,res)
-        #process(page.cropBox,res)
-        #process(page.bleedBox,res)
+        # cut_box(page.trimBox,scale)
+        # cut_box(page.artBox,scale)
+        # cut_box(page.cropBox,scale)
+        # cut_box(page.bleedBox,scale)
         outdata.addPage(page)
 
     output = file(outputname, 'wb')
@@ -47,7 +46,7 @@ def cut_white(inputname, outputname='output.pdf'):
     output.close()
 
 
-def scan_files(directory, prefix=None, postfix=None):
+def scan_files(directory, prefix=None, postfix=None, sub=False):
     """
     scan files under the dir with spec prefix and postfix
     """
@@ -64,12 +63,14 @@ def scan_files(directory, prefix=None, postfix=None):
                     files_list.append(os.path.join(root, f))
             else:
                 files_list.append(os.path.join(root, f))
+        if not sub:
+            break
 
     return files_list
 
 
 def batch_action(indir, outdir):
-    files = scan_files(indir, None, 'pdf')
+    files = scan_files(indir, postfix='pdf')
     print files
 
     for f in files:
@@ -80,17 +81,21 @@ def batch_action(indir, outdir):
         print name
         print path
 
-        id=path.rfind('\\')
-        dir=path[:id]
+        id = path.rfind('\\')
+        dir = path[:id]
         print dir
-        
+
         if not os.path.exists(dir):
             os.makedirs(dir)
-        cut_white(f,path)
+        cut_white(f, path)
+
 
 if __name__ == "__main__":
     inputfile = './input/input.pdf'
+    inputfile = 'F:\\pdf\\dist\\concolic\\concolic2.pdf'
+    inputfile = 'F:\\pdf\\dist\\concolic\\output\\output.pdf'
     outputfile = './output/output.pdf'
+    outputfile = 'F:\\pdf\\dist\\concolic\\output\\output2.pdf'
     cut_white(inputfile, outputfile)
     exit(0)
 
