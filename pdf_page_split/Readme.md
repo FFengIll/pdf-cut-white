c81ed1e296ca9cbd476124fcd641b60d0cd4a6cc
FFengIll <fengyouzheng@gmail.com>
2021-12-13 21:57:36 +0800

    add an extra pdf tool for spliting pdf page.

---
+++ pdf_page_split/Readme.md
@@ -0,0 +1,42 @@
+# 目标
+快速的，自动的将包含多张图的单一pdf，
+拆解为多个独立的pdf，每个pdf仅包含一张图用于latex等用作引用。
+
+# 场景
+- 不打算一一打印图表，一次性打印为一个pdf，包含多个图
+- 确定好图的caption，ref
+  - 写成config
+  - 且与pdf每一页对应
+  - 配置为csv格式，每一行对应每一页，无用的页对应空行，或者''
+- 自动拆分pdf，并输出
+  - 每一张图一个pdf文件，文件名为ref
+  - 输出所有的对应latex语句（template.tex）中可以修改
+
+# 使用
+- python3 -m pip install PyPDF2
+- python3 splitor.py -t figure.tex -c figure.csv -o split/figure testcase.pdf
+  - `-t`选择模板，只有figure和table两类（也可以执行修改）
+  - `-c`选择数据文件
+  - `-o`选择输出的文件夹
+  - 最后是输入的pdf文件
+- python3 splitor.py -t table.tex -c table.csv -o split/table testcase.pdf
+  - 请选用需要分割的pdf文件
+  - 如需要，请对应修改tex模板
+  - 请对应pdf，填写csv文件（直接文本编辑，或在excel中编辑皆可）
+  - 因为实际使用时，图表是分开的，所以分为了两套
+
+# 示例
+- 准备输入文件
+  - pdf
+  - 包含多张图
+  - 每页一张图
+- 准备使用工具
+  - 务必使用python3
+  - pip install（见上）
+  - 准备csv，按照label（也是文件名），caption的格式，与pdf每一页一一对应
+- 运行工具（命令如上）
+  - figure
+  - table
+- 生成结果
+  - pdf文件位于对应文件夹（默认split下）
+  - 对应的tex语句可查看`log.txt`（每次执行都会覆盖，请自行备份）
---
+++ pdf_page_split/splitor.py
@@ -0,0 +1,134 @@
+# coding=utf-8
+import argparse
+import PyPDF2 as pdflib
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+import os
+import sys
+import logging
+
+
+import configparser
+import csv
+
+# from config import config
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", help="output folder", action="store",
+                        default='./output/figure', type=str, dest="output")
+    parser.add_argument("-t", "--template", help="",
+                        action="store", default='figure.tex', dest="template")
+    parser.add_argument("-c", help="config file", action="store",
+                        default='figure.csv', type=str, dest="config")
+    parser.add_argument("--only", help="log and print only", action="store_true",
+                        default=False, dest="only")
+    parser.add_argument(nargs=argparse.REMAINDER, dest="file")
+    return parser
+
+
+def load_template(path):
+    template = ''
+    with open(path) as fd:
+        template = fd.read()
+    print(template)
+    return template
+
+
+def load_config(path):
+    config = []
+    with open(path, 'r', encoding='utf-8') as csvfile:
+        data = csv.reader(csvfile, delimiter=',', quotechar='\'')
+        for row in data:
+            if row[0] == '':
+                config.append(None)
+            else:
+                config.append(row)
+    print(config)
+    return config
+
+    # config = configparser.ConfigParser()
+    # config.read(path, encoding='utf-8')
+    # figures = config['figure']
+    # print(figures)
+    # for key, value in figures.items():
+    #     if value.lower == 'none':
+    #         figures[key] = None
+    # return figures
+
+    # config = []
+    # with open(path) as fd:
+    #     for line in fd:
+    #         line = line.strip()
+    #         if line.startswith('#'):
+    #             continue
+    #         if line.lower() =='none':
+    #             config.append(None)
+    #         else:
+    #             config.append(line)
+    # print(config)
+    # return config
+
+
+def split(path, template, config, output='split', no_pdf=False):
+    """
+    split pdf file, and output multiple new pdf files.
+    """
+    # if inpath==outpath:
+    #     raise Exception('input and output can not be the same!')
+
+    if os.path.exists(output):
+        if not os.path.isdir(output):
+            raise Exception('must be a folder path')
+    else:
+        os.makedirs(output)
+
+    pages = []
+    with open(path, 'rb') as pdf, open('log.txt'.format(output), 'w', encoding='utf-8') as log:
+        reader = PdfFileReader(pdf)
+
+        for i, item in zip(range(reader.getNumPages()), config):
+            if item is None:
+                continue
+
+            (name, caption) = item
+            path = '{}/{}.pdf'.format(output, name)
+
+            print('-'*10)
+            statements = template.format(
+                path=path, label=name, caption=caption)
+            print(statements)
+            log.write(statements+'\n\n')
+
+            page = reader.getPage(i)
+
+            if not no_pdf:
+                writer = PdfFileWriter()
+                writer.addPage(page)
+                with open('{}'.format(path), 'wb') as figure:
+                    writer.write(figure)
+
+
+def main():
+    sys.argv.append('testcase.pdf')
+
+    parser = get_parser()
+    args = parser.parse_args()
+    print(args)
+
+    pdf = args.file[0]
+    template = args.template
+    config = args.config
+    output = args.output
+
+    config = load_config(config)
+    template = load_template(template)
+
+    split(pdf, template, config, output, no_pdf=args.only)
+
+
+if __name__ == "__main__":
+    main()
